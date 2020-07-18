a6d9b6a230766f79b88cb3e1158917aaf4c50aea
Junhyun Kim <me@junbread.win>
2020-07-18 16:30:28 +0900

    mecab 설치 안하도록 변경

--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-
-# install mecab
-bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
