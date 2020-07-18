425b2597b33528f8ba3307c4357b00e71887aac7
Junhyun Kim <me@junbread.win>
2020-07-18 17:19:16 +0900

    conda 환경 있을 경우 우선 사용하도록 변경

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 # create virtual environment and install requirements
-python3 -m venv .venv
-source .venv/bin/activate
+if command -v conda > /dev/null
+then
+    conda activate keyword-extraction
+else
+    python3 -m venv .venv
+    source .venv/bin/activate
+fi
+
 pip install -r requirements.txt
