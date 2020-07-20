481b1f3a6dc350967deeb2f5bab02ba3919477c1
Junhyun Kim <me@junbread.win>
2020-07-20 15:45:51 +0900

    setuptools 설치 옵션 추가

---
+++ setup.py
@@ -0,0 +1,13 @@
+import keyext
+from setuptools import setup, find_packages
+
+setup(
+    name='keyext',
+    version='0.0.1',
+    author='Junhyun Kim',
+    author_email='me@junbread.win',
+    url='https://github.com/2020swchallenge-moai/keyword-extraction',
+    description='Keyword extraction based on TF-IDF',
+    install_requires=['sklearn>=0.23', 'krwordrank'],
+    packages=find_packages()
+)
