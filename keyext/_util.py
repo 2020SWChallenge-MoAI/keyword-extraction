41aa005294d13a2eb811253f7070ab89f7fdc944
Junhyun Kim <me@junbread.win>
2020-07-21 23:12:23 +0900

    ordered combination 추가

--- keyext/_util.py
+++ keyext/_util.py
@@ -3,3 +3,15 @@ def remove_duplicate(list):
     seen_add = seen.add
 
     return [x for x in list if not (x in seen or seen_add(x))]
+
+
+def ordered_combination(list, range):
+    result = []
+    for i in range:
+        result.extend([
+            [element for idx, element in sorted(subset)]
+            for subset
+            in combinations(enumerate(list), i)
+        ])
+
+    return result
